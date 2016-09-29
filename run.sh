c7ffda382200d982b7f3a6180c98f4f14b16b2a9
System Administrator <root@studios-MacBook-Pro.local>
2016-09-29 08:07:27 +0100

    init

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 NAME=$1
 FOLDER=$2
 
-CONTAINER=`docker run -d -p 8888:8888 -v $FOLDER:/home/c2o/notebooks c2ostudio/$NAME-starter`
+CONTAINER=`docker run -d -p 8888:8888 -v $FOLDER:/home/c2o/notebooks c2ostudio/$NAME-base`
 echo $CONTAINER
